ea22259b5a4c6a5cabdd7c35c0586aade5f7abab
lightsing <light.tsing@gmail.com>
2018-06-12 11:58:18 +0800

    move to static folder

---
+++ static/dashboard.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  $('#mods input[type="button"]').click(function() {
+    var e = $(this);
+    var name = e.attr('name');
+    var action = e.attr('value');
+    
+  });
+});
